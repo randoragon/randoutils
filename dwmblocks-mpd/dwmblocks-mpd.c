65c1781477e8abb0d3f33930ee2b23a72a909842
Randoragon <randoragongamedev@gmail.com>
2020-07-04 22:35:37 +0200

    Init dwmblocks-mpd
    Nothing in particular is ready yet, for now just initializing.

---
+++ dwmblocks-mpd/dwmblocks-mpd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <freetype2/ft2build.h>
+#include FT_FREETYPE_H
+
+#define FONT "dejavu sans:size=9:antialias=true"
+
+int main(int argc, char **argv)
+{
+    printf("hello there\n");
+    return 0;
+}
