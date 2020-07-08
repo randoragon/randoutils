fbea714dd51a60aba209b2f778f65495b2ded345
Randoragon <randoragongamedev@gmail.com>
2020-07-08 06:48:55 +0200

    [dwmbmpd] Add install target to Makefile

--- dwmbmpd/config.mk
+++ dwmbmpd/config.mk
@@ -1,5 +1,6 @@
 # general config
 CC = cc
+DEST = ~/.scripts/dwmblocks
 
 # freetype2
 FT_INCS = -I/usr/include/freetype2
@@ -18,5 +19,5 @@ XLIB_INCS =
 XLIB_LIBS = -lX11
 
 OBJS = dwmbmpd.c utf8.c utf8.h
-INCS = ${FT_INCS} ${FC_INCS} ${MPD_INCS} ${XLIB_INCS}
-LIBS = ${FT_LIBS} ${FC_LIBS} ${MPD_LIBS} ${XLIB_LIBS}
+INCS = $(FT_INCS) $(FC_INCS) $(MPD_INCS) $(XLIB_INCS)
+LIBS = $(FT_LIBS) $(FC_LIBS) $(MPD_LIBS) $(XLIB_LIBS)
