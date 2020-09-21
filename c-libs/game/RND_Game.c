3d45a6130c7a07847a1a6b1efb3dab1cb1cecb88
Randoragon <randoragongamedev@gmail.com>
2020-09-21 23:10:41 +0200

    [c-libs/game] Implement objects, instances, event handlers
    I've decided to maintain an OVERVIEW.MD file which basically acts as a
    comprehensive guide or documentation to the whole library. I do this not
    so much to help others (I'm not expecting anyone beside me to use this
    library), but to make it easier for myself to remember the goals behind
    every implementation and keep track of what's missing.

--- c-libs/game/RND_Game.c
+++ c-libs/game/RND_Game.c
@@ -1,1 +1,73 @@
+#include <RND_LinkedList.h>
+#include <stdio.h>
+
 #include "RND_Game.h"
+
+// Variable Definitions
+void *objects[RND_OBJECT_MAX];
+RND_LinkedList *instances;
+size_t object_sizeof[RND_OBJECT_MAX] = {0};
+RND_Handlers ctors = {0},
+             dtors = {0},
+             steps = {0},
+             draws = {0};
+
+// Function Definitions
+void RND_gameInit()
+{
+    instances = RND_linkedListCreate();
+}
+
+void RND_gameCleanup()
+{
+    RND_linkedListDestroy(&instances, RND_gameInstanceDtor);
+}
+
+void  RND_gameObjectAdd(RND_GameObjectIndex index, size_t size)
+{
+    if (object_sizeof[index]) {
+        fprintf(stderr, "RND_gameObjectAdd: object index %u is already taken!\n", index);
+        return;
+    }
+    object_sizeof[index] = size;
+}
+
+void *RND_gameInstanceSpawn(RND_GameObjectIndex index)
+{
+    if (!objects[index]) {
+        fprintf(stderr, "RND_gameInstanceSpawn: object index %u not found in the objects array\n", index);
+        return NULL;
+    }
+    RND_GameInstance *new;
+    if (!(new = (RND_GameInstance*)malloc(sizeof(RND_GameInstance)))) {
+        fprintf(stderr, "RND_gameInstanceSpawn: malloc\n");
+        return NULL;
+    }
+    new->id = 0; // TODO: instance ID management
+    new->index = index;
+    if (!(new->data = malloc(object_sizeof[index]))) {
+        fprintf(stderr, "RND_gameInstanceSpawn: malloc\n");
+        free(new);
+        return NULL;
+    }
+    RND_linkedListAdd(&instances, new);
+    return new->data;
+}
+
+int RND_gameInstanceDtor(void *data)
+{
+    RND_GameInstance *inst = data;
+    return dtors[inst->index]? dtors[inst->index](inst->data) : 0;
+}
+
+void RND_gameRunHandlers(RND_Handlers handlers)
+{
+    for (RND_LinkedList *elem = instances; elem; elem = elem->next) {
+        RND_GameInstance *inst = elem->data;
+        if (handlers[inst->index]) {
+            if (handlers[inst->index](inst->data)) {
+                fprintf(stderr, "RND_gameRunHandlers: handler %p returned non-0 for instance %u of object %u\n", handlers, inst->id, inst->index);
+            }
+        }
+    }
+}
--- c-libs/game/RND_Game.h
+++ c-libs/game/RND_Game.h
@@ -1,4 +1,41 @@
 #ifndef RND_GAME_H
 #define RND_GAME_H
 
+#include <stdlib.h>
+#include <stdint.h>
+#include <RND_LinkedList.h>
+
+// Macros
+#define RND_OBJECT_MAX   0xffff
+#define RND_INSTANCE_MAX 0xffff
+
+// Type declarations
+typedef uint16_t RND_GameObjectIndex;
+typedef uint16_t RND_GameInstanceId;
+typedef int (*RND_Handlers[RND_OBJECT_MAX])(void*);
+typedef struct RND_GameInstance RND_GameInstance;
+
+// Variable Declarations
+extern void *objects[RND_OBJECT_MAX];
+extern RND_LinkedList *instances;
+extern size_t object_sizeof[RND_OBJECT_MAX];
+extern RND_Handlers ctors, dtors, steps, draws;
+
+
+// Structures
+struct RND_GameInstance
+{
+    RND_GameInstanceId id;
+    RND_GameObjectIndex index;
+    void *data;
+};
+
+// Functions
+void  RND_gameInit();
+void  RND_gameCleanup();
+void  RND_gameObjectAdd(RND_GameObjectIndex index, size_t size);
+void *RND_gameInstanceSpawn(RND_GameObjectIndex index);
+int   RND_gameInstanceDtor(void *data);
+void  RND_gameRunHandlers(RND_Handlers handlers);
+
 #endif
