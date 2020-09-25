f11793d54c24139f1c4f4b3e0e09a38d3a276b48
Randoragon <randoragongamedev@gmail.com>
2020-09-25 14:26:19 +0200

    [c-libs/errmsg] Print function name
    Also updated the makefile.

--- c-libs/errmsg/RND_ErrMsg.h
+++ c-libs/errmsg/RND_ErrMsg.h
@@ -5,21 +5,21 @@
 
 #define RND_INFO(...) \
     do { \
-        fprintf(stderr, "[INFO]  (%s:%d) ", __FILE__, __LINE__); \
+        fprintf(stderr, " [INFO] (%s:%d) (%s) ", __FILE__, __LINE__, __func__); \
         fprintf(stderr, __VA_ARGS__); \
         putc('\n', stderr); \
     } while (0)
 
 #define RND_WARN(...) \
     do { \
-        fprintf(stderr, "[WARN]  (%s:%d) ", __FILE__, __LINE__); \
+        fprintf(stderr, " [WARN] (%s:%d) (%s) ", __FILE__, __LINE__, __func__); \
         fprintf(stderr, __VA_ARGS__); \
         putc('\n', stderr); \
     } while (0)
 
 #define RND_ERROR(...) \
     do { \
-        fprintf(stderr, "[ERROR] (%s:%d) ", __FILE__, __LINE__); \
+        fprintf(stderr, "[ERROR] (%s:%d) (%s) ", __FILE__, __LINE__, __func__); \
         fprintf(stderr, __VA_ARGS__); \
         putc('\n', stderr); \
     } while (0)
