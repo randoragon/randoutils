0b35766d961cbf304f28ce0e3e9eeb4de5b53363
Randoragon <randoragongamedev@gmail.com>
2020-09-27 11:13:08 +0200

    [c-libs/queue] Add RND_queueRemove function
    Copied from the linked list implementation, because, as it turns out,
    being able to remove an arbitrary element from within a queue comes in
    handy sometimes.

--- c-libs/queue/RND_Queue.c
+++ c-libs/queue/RND_Queue.c
@@ -52,6 +52,48 @@ int RND_queuePop(RND_Queue **queue, int (*dtor)(void*))
     return 0;
 }
 
+int RND_queueRemove(RND_Queue **queue, size_t index, int (*dtor)(void *))
+{
+    if (!*queue) {
+        RND_WARN("the queue is already empty");
+        return 1;
+    }
+    if (index == 0) {
+        int error;
+        if (dtor && (error = dtor((*queue)->data))) {
+            RND_ERROR("dtor %p returned %d for data %p", dtor, error, (*queue)->data);
+            return 2;
+        }
+        RND_Queue *tmp = *queue;
+        *queue = (*queue)->next;
+        free(tmp);
+    } else {
+        RND_Queue *prev = *queue;
+        for (int i = 0; i < index - 1; i++) {
+            if (prev->next) {
+                prev = prev->next;
+            } else {
+                RND_ERROR("index %lu out of bounds (size %lu)", index, RND_queueSize(queue));
+                return 3;
+            }
+        }
+        if (!prev->next) {
+            RND_ERROR("index %lu out of bounds (size %lu)", index, RND_queueSize(queue));
+            return 3;
+        }
+        int error;
+        if (dtor && (error = dtor(prev->next->data))) {
+            RND_ERROR("dtor %p returned %d for data %p", dtor, error, prev->next->data);
+            return 4;
+        }
+        RND_Queue *tmp;
+        tmp = prev->next->next;
+        free(prev->next);
+        prev->next = tmp;
+    }
+    return 0;
+}
+
 int RND_queueClear(RND_Queue **queue, int (*dtor)(void*))
 {
     RND_Queue *i = *queue;
