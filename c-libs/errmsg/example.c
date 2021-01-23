b11a902f1ee91e7a857ca69a471f3386f8731bbf
Randoragon <randoragongamedev@gmail.com>
2021-01-23 20:32:15 +0100

    Migrate c-libs to another repository
    I never intended randoutils to be a repository for libraries - when
    creating the c-libs directory initially I didn't expect it to grow so
    large, so it only makes sense to "detach" it into a separate repo.
    See https://github.com/randoragon/rnd-libs

--- c-libs/errmsg/example.c
+++
@@ -1,9 +0,0 @@
-#include <RND_ErrMsg.h>
-
-int main(int argc, char **argv)
-{
-    RND_INFO("This is an info message: %d", 1);
-    RND_WARN("This is a warning message: %d", 2);
-    RND_ERROR("This is an error message: %d", 3);
-    return 0;
-}
--- c-libs/game/example.h
+++
@@ -1,8 +0,0 @@
-// Object indices
-#include "example_obj_indices.h"
-
-// Object prototypes
-#include "example_obj_player.h"
-#include "example_obj_wall.h"
-#include "example_obj_enemy.h"
-
